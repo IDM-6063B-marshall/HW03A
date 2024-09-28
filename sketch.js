61975a915f193b7b6e9278565b2f0b9efa7c639e
Jubilee Marshall <jam1703@nyu.edu>
2024-09-28 16:37:36 -0400

    Update sketch.js
    Prettified

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
 
   // draw a column of rows of little circles
   for (let y = 0; y < height; y += 50) {
-    for (let x = 0; x < width; x +=50) ellipse(x, y, 10, 10);
+    for (let x = 0; x < width; x += 50) ellipse(x, y, 10, 10);
   }
 
   // draw a column of rows of big circles
@@ -14,13 +14,9 @@ function setup() {
     for (let x = 0; x < width; x += 100) ellipse(x, y, 25, 25);
   }
 
-// draw a second column of rows of big circles
-translate (50, 50)
-for (let y = 0; y < height; y +=100) {
-  for (let x = 0; x < width; x += 100) ellipse(x, y, 25, 25);
-}
-
-
-
+  // draw a second column of rows of big circles
+  translate(50, 50);
+  for (let y = 0; y < height; y += 100) {
+    for (let x = 0; x < width; x += 100) ellipse(x, y, 25, 25);
   }
-
+}
